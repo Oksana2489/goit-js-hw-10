4be669ce12036e61d41a76e1703111d4bde36084
Oksana2489 <viva.forever53@gmail.com>
2023-07-18 01:25:22 +0300

    Update cat-api.js

---
+++ src/cat-api.js
@@ -0,0 +1,13 @@
+const API_key = 'live_ quhXgo8hYOvzHwt9xGRV9NRNMaWB2h yJsjR5WRa5R0yAolibd9QfhjSTEKT4 rHr6';
+
+function fetchBreeds() {
+    return fetch('https://api.thecatapi.com/v1/breeds')
+        .then(response => response.json());
+}
+
+function fetchCatByBreed(breedId){
+    return fetch(`https://api.thecatapi.com/v1/images/search?limit=1&breed_ids=${breedId}&api_key=${API_key}`)
+        .then(data => data.json());
+}
+
+export { fetchBreeds, fetchCatByBreed };
