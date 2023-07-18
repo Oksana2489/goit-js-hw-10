713616c4c874199a6c93ab95327c649b0cc3f69a
Oksana2489 <108237055+Oksana2489@users.noreply.github.com>
2023-07-18 21:24:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,50 +1,89 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import Notiflix from 'notiflix';
 import SlimSelect from 'slim-select'
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
-new SlimSelect({
-  select: '#selectElement'
-})
+const breedSelect = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+const catInfo = document.querySelector('.cat-info');
+new SlimSelect('#breed-select', {
+  contentPosition: 'relative',
+});
+fetchBreeds()
+  .then(data => {
+    let breed;
 
-const refs = {
-    select: document.querySelector('.breed-select'),
-    catInfo: document.querySelector('.cat-info'),
-    loader: document.querySelector('.loader'),
-    error: document.querySelector('.error')
+    breed = data.map(breeds => ({
+      value: breeds.id,
+      label: breeds.name,
+      description: breeds.description,
+      temperament: breeds.temperament,
+    }));
+  let markup = `<option value=''>Choose your cat</option>`;
+
+    breed.forEach(breed => {
+      markup += `<option value='${breed.value}'>${breed.label}</option>`;
+    });
+
+    breedSelect.innerHTML = markup;
+
+    breedSelect.addEventListener('change', handleChoice);
+
+    function handleChoice(evt) {
+      catInfo.innerHTML = '';
+      showLoader();
+      evt.preventDefault();
+
+      let breedId = evt.target.value;
+      const selectedBreed = breed.find(breed => breed.value === breedId);
+
+      fetchCatByBreed(breedId)
+        .then(el => {
+          let markupLi = '';
+
+          el.forEach(img => {
+            markupLi += `<li class="cat-picture"><img src=${img.url}></img></li>`;
+          });
+          let markupUl = '<ul class="cat-list">' + markupLi + '</ul>';
+
+          const newHtml = `<div class='cat-descr'>
+          <p class='name'>${selectedBreed.label}</p>
+          <p class='description'>${selectedBreed.description}</p>
+          <p class='features'>${selectedBreed.temperament}</p>
+          </div >`;
+
+          if (selectedBreed) {
+            catInfo.innerHTML += markupUl + newHtml;
+          }
+
+          hideLoader();
+        })
+        .catch(err => {
+          showError();
+        });
+    }
+  })
+  .catch(err => {
+    Notiflix.Notify.failure(
+      'Oops! Something went wrong! Try reloading the page!'
+    );
+  });
+
+function showLoader() {
+  loader.style.display = 'block';
 }
 
-refs.error.setAttribute('hidden', '');
+function hideLoader() {
+  loader.style.display = 'none';
+}
 
-fetchBreeds()
-    .then(cat => {
-        const murkup = cat.map(c => `<option value="${c.id}">${c.name}</option>`).join('');     
-        refs.select.innerHTML = murkup;      
-    })
-    .catch(error => {
-        refs.error.removeAttribute('hidden', '');
-        console.log(error)
-    });
-        
-refs.select.addEventListener('change', (e) => {
-   refs.loader.removeAttribute('hidden', '');
-        fetchCatByBreed(e.target.value)
-            .then(cat => {             
-                refs.catInfo.innerHTML = createMurkup(cat);
-                refs.loader.setAttribute('hidden', '');
-            })
-            .catch(error => {
-                refs.error.removeAttribute('hidden', '');
-                console.log(error)
-            });
-});
-
-function createMurkup(cat) { 
-    const murkup = cat.map(c =>
-        `   <img class="cat-img" src="${c.url}" width="340"/>
-                <div class="cat-description">
-                <h2>${c.breeds[0].name}</h2>
-                <p>${c.breeds[0].description}</p>
-                <p><h3>Temperament:</h3> ${c.breeds[0].temperament}</p>
-            </div>                        
-        `).join(''); 
-    return murkup;
-}    
+function showError() {
+  if (breedSelect.value === '') {
+    hideLoader();
+    return Notiflix.Notify.warning('Choose a cat breed from the list');
+  } else {
+      Notiflix.Notify.failure(
+          'Oops! Something went wrong! Try reloading the page!');
+  };
+}
+browserslist.clearCaches()  
